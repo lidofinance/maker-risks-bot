021c13465d8a4d052e33276683d5a8451c078ec1
madlabman <10616301+madlabman@users.noreply.github.com>
2023-01-26 10:22:35 +0100

    feat: expose provider to RPC metrics

--- src/bot/eth.py
+++ src/bot/eth.py
@@ -8,10 +8,9 @@ from pathlib import Path
 
 from web3 import HTTPProvider, Web3
 from web3.contract import Contract
-from web3_multi_provider import MultiProvider
 
 from .config import FALLBACK_NODE_ENDPOINT, NODE_ENDPOINT
-from .middleware import chain_id_mock, metrics_collector, retryable
+from .middleware import chain_id_mock, construct_fallback_provider_middleware, metrics_collector, retryable
 
 log = logging.getLogger(__name__)
 
@@ -25,20 +24,11 @@ CROPPER_ADDRESS = "0x8377CD01a5834a6EaD3b7efb482f678f2092b77e"
 VAT_ADDRESS = "0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B"
 
 
-if FALLBACK_NODE_ENDPOINT:
-    provider = MultiProvider(
-        [
-            NODE_ENDPOINT,
-            FALLBACK_NODE_ENDPOINT,
-        ]
-    )
-else:
-    # use an usual provider instead of MultiProvider to be able to switch seamlessly in case of errors
-    provider = HTTPProvider(NODE_ENDPOINT)
-
-w3 = Web3(provider)
+w3 = Web3(HTTPProvider(NODE_ENDPOINT))
 w3.middleware_onion.add(metrics_collector)
 w3.middleware_onion.add(retryable)
+if FALLBACK_NODE_ENDPOINT:
+    w3.middleware_onion.add(construct_fallback_provider_middleware(w3, FALLBACK_NODE_ENDPOINT))
 w3.middleware_onion.add(chain_id_mock)
 
 
--- src/bot/middleware.py
+++ src/bot/middleware.py
@@ -5,6 +5,7 @@
 import logging
 from typing import Any, Callable
 
+from eth_typing import URI  # type: ignore
 from requests import HTTPError, Response
 from retry.api import retry_call
 from web3 import Web3
@@ -17,7 +18,7 @@ log = logging.getLogger(__name__)
 
 
 def chain_id_mock(
-    make_request: Callable[[RPCEndpoint, Any], RPCResponse], w3: "Web3"
+    make_request: Callable[[RPCEndpoint, Any], RPCResponse], _: Web3
 ) -> Callable[[RPCEndpoint, Any], RPCResponse]:
     """Constructs a middleware which mock eth_chainId method call response"""
 
@@ -44,13 +45,15 @@ def metrics_collector(
     """Constructs a middleware which measure requests parameters"""
 
     def middleware(method: RPCEndpoint, params: Any) -> RPCResponse:
+        rpc_domain = _get_provider_domain_from_w3(w3)
 
         try:
-            with ETH_RPC_REQUESTS_DURATION.time():
+            with ETH_RPC_REQUESTS_DURATION.labels(provider=rpc_domain).time():
                 response = make_request(method, params)
         except HTTPError as ex:
             failed: Response = ex.response
             ETH_RPC_REQUESTS.labels(
+                provider=rpc_domain,
                 method=method,
                 code=failed.status_code,
             ).inc()
@@ -63,6 +66,7 @@ def metrics_collector(
             if isinstance(error, dict):
                 code = error.get("code") or code
             ETH_RPC_REQUESTS.labels(
+                provider=rpc_domain,
                 method=method,
                 code=code,
             ).inc()
@@ -72,8 +76,43 @@ def metrics_collector(
     return middleware
 
 
+def construct_fallback_provider_middleware(w3: Web3, fallback: URI) -> Callable:
+
+    assert isinstance(w3.provider, Web3.HTTPProvider)
+    main_endpoint = w3.provider.endpoint_uri
+    assert main_endpoint, "no endpoint URI found"
+    endpoints = (main_endpoint, fallback)
+
+    def fallback_provider(
+        make_request: Callable[[RPCEndpoint, Any], RPCResponse], _: Web3
+    ) -> Callable[[RPCEndpoint, Any], RPCResponse]:
+        """Constructs a middleware which measure requests parameters"""
+
+        def middleware(method: RPCEndpoint, params: Any) -> RPCResponse:
+
+            try:
+                return make_request(method, params)
+            except Exception as e:  # pylint: disable=W0703
+                log.warning("Request to provider has been failed: %s", e)
+                switch_endpoint()
+                return make_request(method, params)
+
+        return middleware
+
+    def switch_endpoint() -> None:
+        w3.provider.endpoint_uri = next_endpoint()  # type: ignore
+        rpc_domain = _get_provider_domain_from_w3(w3)
+        log.warning("Switching to provider %s", rpc_domain)
+
+    def next_endpoint() -> URI:
+        idx = endpoints.index(w3.provider.endpoint_uri)  # type: ignore
+        return endpoints[(idx + 1) % len(endpoints)]
+
+    return fallback_provider
+
+
 def retryable(
-    make_request: Callable[[RPCEndpoint, Any], RPCResponse], w3: "Web3"
+    make_request: Callable[[RPCEndpoint, Any], RPCResponse], _: Web3
 ) -> Callable[[RPCEndpoint, Any], RPCResponse]:
     """Constructs a middleware which retries requests to the endpoint"""
 
@@ -87,3 +126,10 @@ def retryable(
         )
 
     return middleware
+
+
+def _get_provider_domain_from_w3(w3: Web3) -> str:
+    """Get provider domain from Web3 object"""
+
+    uri = getattr(w3.provider, "endpoint_uri", "unknown")
+    return uri.split("://")[-1].split("/")[0].split(":")[0]
