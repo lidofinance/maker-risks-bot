bf9d344853206d108f4950faca43c6639d99a55d
Vladimir Malyutin <first-leon@yandex.ru>
2023-09-20 13:52:04 +0700

    Added environment variables HTTP_REQUESTS_RETRY and HTTP_REQUESTS_DELAY

--- src/bot/consts.py
+++ src/bot/consts.py
@@ -1,8 +1,17 @@
 """Protocol defined consts"""
 
+import os
+
 RAY_DECIMALS = 27
 RAD_DECIMALS = 45
 WAD_DECIMALS = 18
 
-HTTP_REQUESTS_RETRY = 3
-HTTP_REQUESTS_DELAY = 3
+if os.getenv("HTTP_REQUESTS_RETRY"):
+    HTTP_REQUESTS_RETRY = int(os.getenv("HTTP_REQUESTS_RETRY"))
+else:
+    HTTP_REQUESTS_RETRY = 3
+
+if os.getenv("HTTP_REQUESTS_DELAY"):
+    HTTP_REQUESTS_DELAY = int(os.getenv("HTTP_REQUESTS_DELAY"))
+else:
+    HTTP_REQUESTS_DELAY = 3
